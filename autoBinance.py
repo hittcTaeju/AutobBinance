0b8bde1cf54820cb242c9f93dbc8f4ec711a6694
taeju <taejoo0112@naver.com>
2024-04-04 01:59:40 +0900

    완성! 근데 되는 지 모르겠음

--- autoBinance.py
+++ autoBinance.py
@@ -1,5 +1,9 @@
+import datetime
+import time
+import pandas as pd
 import ccxt
 import pprint
+import larry1
 
 # API 정보 파일 열기
 f = open("./binance.key")
@@ -17,8 +21,36 @@ binance = ccxt.binance(config={
     }
 })
 symbol = "BTC/USDT"
+position = {
+    'type' :None,
+    'amount' : 0
+}
+
+op_mode =True
+#if now.hour == 12 and now.minute == and (20 <= now.second < 30):  # 목표가 갱신
+long_target, short_target = larry1.cal_target(binance, symbol)
+balance = binance.fetch_balance()
+usdt = balance['total']['USDT']
+#time.sleep(10)
 
 while True:
-    #current price
+    now = datetime.datetime.now()
+
+    if now.hour == 8 and now.minute == 50 and (0 <= now.second < 10):#포지션 종료. 청산
+        if op_mode and position['type'] is not None:
+            position=larry1.exit_position(binance,symbol,position)
+            op_mode=False
+
+    #current price, 구매 가능 수량
+    btc =binance.fetch_ticker(symbol=symbol)
+    cur_price = btc['last']
+    amount =larry1.cal_amount(usdt,cur_price)
+
+    if op_mode and position['type'] is None:#진입
+        position=larry1.enter_position(binance,symbol,cur_price,long_target,short_target,amount,position)
+
+    print(now,cur_price,'long target: ',long_target,'short target: ',short_target,position)
+    time.sleep(1)
+
 
 
---
+++ larry1.py
@@ -0,0 +1,60 @@
+import ccxt
+import pandas as pd
+import math
+
+
+f = open("./binance.key")
+lines = f.readlines()
+api_key = lines[0].strip()
+secret = lines[1].strip()
+f.close()
+symbol = "BTC/USDT"
+
+
+def cal_target(binance,symbol):
+    # 목표가격 계산
+    btc_ohlcv = binance.fetch_ohlcv(
+        symbol=symbol,
+        timeframe='1d',
+        since=None,
+        limit=10
+    )
+    df = pd.DataFrame(
+        data=btc_ohlcv,
+        columns=['datetime', 'open', 'high', 'low', 'close', 'volume']
+    )
+    df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+    df.set_index('datetime', inplace=True)
+    yesterday = df.iloc[-2]
+    today = df.iloc[-1]
+    long_target = today['open'] + (yesterday['high'] - yesterday['low']) * 0.5
+    short_target = today['open'] - (yesterday['high'] - yesterday['low']) * 0.5
+    return (long_target,short_target)
+def cal_amount(usdt_balance,cur_price):
+    portion = 0.1
+    usdt_trade = usdt_balance*portion
+    amount = math.floor((usdt_trade*1000000)/cur_price)/1000000
+    return amount
+def enter_position(binance,symbol,cur_price,long_target,short_target,amount,position):
+    if cur_price>long_target:#롱 진입
+        position['type'] = 'long'
+        position['amount'] = amount
+        binance.create_market_buy_order(symbol=symbol,amount=amount)
+        print("ENTER LONG")
+    elif cur_price<short_target: #숏진입
+        position['type'] = 'short'
+        position['amount'] = amount
+        binance.create_market_sell_order(symbol=symbol,amount=amount)
+        print("ENTER SHORT")
+    return position
+def exit_position(binance,symbol,position):
+    amount = position['amount']
+    if position['type'] =='long':
+        binance.create_market_sell_order(symbol=symbol,amount=amount)
+        position['type']=None
+    elif position['type']=='short':
+        binance.create_market_buy_order(symbol=symbol, amount=amount)
+        position['type']=None
+    position['amount']=0
+    return position
+#print(cal_target(symbol))
